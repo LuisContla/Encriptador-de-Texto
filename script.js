eace1fb6e6fd3aba756d8916fb89154fcf2a3f18
Luis Contla <61216373+LuisContla@users.noreply.github.com>
2024-08-01 20:51:24 -0600

    Funcionalidades
    Se añadieron las funcionalidades de encriptar y desencriptar el texto, además de copiar el contenido encriptado al portapapeles

--- script.js
+++ script.js
@@ -21,27 +21,50 @@ function validar(op) {
                 //El texto se encriptará
                 alert("Encriptando...");
                 encriptar(texto, resultado);
-            }else{
+            } else {
                 //El texto se desencriptará
                 alert("Desencriptando...")
                 desencriptar(texto, resultado);
             }
 
-        }else{
+        } else {
             //No se cumplen las especificaciones
             alert("El texto no cumple las especificaciones");
         }
 
-    }else{
+    } else {
         //No hay contenido en el textarea
         alert("Debe haber contenido en el texto");
     }
 }
 
-function encriptar(texto, resultado){
-    resultado.children[0].textContent = texto;
+function encriptar(texto, resultado) {
+    // Usaré la función replace para cambiar las vocales con las llaves de encriptación
+    let txtEncriptado = texto.replace(/e/g, "enter")
+        .replace(/i/g, "imes")
+        .replace(/a/g, "ai")
+        .replace(/o/g, "ober")
+        .replace(/u/g, "ufat");
+    resultado.children[0].textContent = txtEncriptado;
 }
 
-function desencriptar(texto, resultado){
-    resultado.children[0].textContent = texto;
+function desencriptar(texto, resultado) {
+    // Usaré la función replace para cambiar las llaves de encriptación con las vocales correspondientes
+    let txtDesecriptado = texto.replace(/enter/g, "e")
+        .replace(/imes/g, "i")
+        .replace(/ai/g, "a")
+        .replace(/ober/g, "o")
+        .replace(/ufat/g, "u");
+    resultado.children[0].textContent = txtDesecriptado;
+}
+
+const copiarContenido = async () => {
+    // Obtengo el texto del text area del resultado
+    let txtCopiar = document.getElementById('txt-res').value;
+    try {
+        await navigator.clipboard.writeText(txtCopiar);
+        alert('Contenido copiado al portapapeles');
+    } catch (err) {
+        alert('Error al copiar: ', err);
+    }
 }
